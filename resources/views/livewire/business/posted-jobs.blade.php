1d0d7b0bc2beacb9834c5b50131da254adf25570
Furukuku <perezkmlagmay@gmail.com>
2023-12-06 18:13:18 +0800

    Enhanced the Terminology of category of job posting

--- resources/views/livewire/business/posted-jobs.blade.php
+++ resources/views/livewire/business/posted-jobs.blade.php
@@ -5,10 +5,10 @@
 
   <div class="d-flex justify-content-center mt-4">
     <div wire:click="offers" class="d-flex align-items-center px-3 py-2 border-bottom requested-docs-tabs {{ $tab === 'offers' ? 'border border-bottom-0 rounded-top' : '' }}">
-      <p class="m-0 text-center {{ $tab === 'offers' ? 'fw-semibold' : '' }}">Open Jobs</p>
+      <p class="m-0 text-center {{ $tab === 'offers' ? 'fw-semibold' : '' }}">Vacancies</p>
     </div>
     <div wire:click="done" class="d-flex align-items-center px-3 py-2 border-bottom requested-docs-tabs {{ $tab === 'done' ? 'border border-bottom-0 rounded-top' : '' }}">
-      <p class="m-0 text-center {{ $tab === 'done' ? 'fw-semibold' : '' }}">Closed Jobs</p>
+      <p class="m-0 text-center {{ $tab === 'done' ? 'fw-semibold' : '' }}">Occupied</p>
     </div>
   </div>
   @if ($tab === 'offers')
